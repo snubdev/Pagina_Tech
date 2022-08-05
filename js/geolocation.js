68c4a91504dcd26b85bab941cfa3404932d4d749
Maicon Leandro <snubhousedev@gmail.com>
2022-08-05 03:31:18 -0300

    Add Pagina Tech

---
+++ js/geolocation.js
@@ -0,0 +1,12 @@
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+function showLocation(position){
+    const long = position.coords.longitude;
+    const lat = position.coords.latitude;
+
+    const url = `https://maps.google.com/maps?q=${lat},${long}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+
+    window.open(url, 'map')
+}
